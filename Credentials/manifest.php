28578616fec21f4816c979c35bb04b28cb06769b
rossdotparker <rossdotparker@gmail.com>
2015-09-09 11:19:48 +0800

    v1.0.01

--- Credentials/manifest.php
+++ Credentials/manifest.php
@@ -25,7 +25,7 @@ $description="Credentials is a module for storing student login details, and mak
 $entryURL="credentials.php" ;
 $type="Additional" ;
 $category="Admin" ;
-$version="1.0.00" ;
+$version="1.0.01" ;
 $author="Ross Parker" ;
 $url="http://rossparker.org" ;
 
