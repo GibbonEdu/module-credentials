824da11add80a27b566f29cc7f63a56f7085e4ee
Harry Merrett <hmerrett@ichk.edu.hk>
2021-06-10 10:18:24 +0800

    Version number update

--- Credentials/manifest.php
+++ Credentials/manifest.php
@@ -25,7 +25,7 @@ $description = 'Credentials is a module for storing student login details, and m
 $entryURL = 'credentials.php';
 $type = 'Additional';
 $category = 'Admin';
-$version = '3.1.01';
+$version = '3.1.02';
 $author = 'Ross Parker';
 $url = 'http://rossparker.org';
 
